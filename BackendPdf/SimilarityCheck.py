24c1da3f2e14ef702928f71b0b9579c39a57eb07
vaniNandyala <vanis7522@gmail.com>
2026-06-04 21:52:17 +0530

    Update SimilarityCheck.py

--- BackendPdf/SimilarityCheck.py
+++ BackendPdf/SimilarityCheck.py
@@ -1,31 +1,55 @@
-
 from flask import Flask, request, jsonify
-from sentence_transformers import SentenceTransformer, util
+import requests
 import os
 
 app = Flask(__name__)
 
-# Initialize the SentenceTransformer model globally for efficiency
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+HF_TOKEN = os.environ.get("HF_TOKEN")
+
+API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
+
+headers = {
+    "Authorization": f"Bearer {HF_TOKEN}"
+}
 
 @app.route('/api/similarity', methods=['POST'])
 def calculate_similarity():
     try:
         data = request.json
+
         transcription_texts = data.get('transcription_texts')
         pdf_texts = data.get('pdf_texts')
 
         if not transcription_texts or not pdf_texts:
-            return jsonify({'error': 'Both transcription_texts and pdf_texts are required'}), 400
+            return jsonify({
+                "error": "Both transcription_texts and pdf_texts are required"
+            }), 400
 
-        # Calculate embeddings and cosine similarity
-        embeddings = model.encode([transcription_texts, pdf_texts], convert_to_tensor=True)
-        cosine_score = util.pytorch_cos_sim(embeddings[0], embeddings[1]).item()
+        payload = {
+            "inputs": {
+                "source_sentence": transcription_texts,
+                "sentences": [pdf_texts]
+            }
+        }
 
-        return jsonify({'transcription_texts': transcription_texts, 'pdf_texts': pdf_texts, 'similarity_score': cosine_score})
+        response = requests.post(
+            API_URL,
+            headers=headers,
+            json=payload
+        )
+
+        result = response.json()
+
+        return jsonify({
+            "similarity_score": result[0]
+        })
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
-if __name__ == '__main__':
+        return jsonify({
+            "error": str(e)
+        }), 500
+
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
